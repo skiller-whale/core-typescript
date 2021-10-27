5b7ca54c15ab0f05a09b3b2beb1e40bf598d82bd
Amyas Merivale <merivale@gmail.com>
2021-10-27 08:25:41 +0200

    Update object types exercises

--- exercises/object_types/exercises.ts
+++ exercises/object_types/exercises.ts
@@ -1,22 +1,21 @@
 import { formatName } from './lib'
 
-print({ name: 'tomatoes', quantity: 800, measurement: 'grams' }, 1)
-print({ name: 'basil', quantity: 5, measurement: 'leaves' }, 1)
+print({ name: 'tomatoes', quantity: 800, measurement: 'grams' }, 2)
+print({ name: 'basil', quantity: 5, measurement: 'leaves' }, 2)
 
 // Prints an ingredient to the console. Returns a string containing
 // the number of servings.
 function print (ingredient: any, servings: number): string {
-  ingredient.quantity = ingredient.quantity * servings
-
-  let firstLine =
-    `    ${formatName(ingredient.name)}: ${ingredient.amount} ${ingredient.measurement}`
-  let secondLine = `        (${ingredient.amount} ${ingredient.measurement} per person)`
+  let firstLine = `${formatName(ingredient.name)}:`
+  let secondLine = `  ${ingredient.amount} ${ingredient.measurement} per person`
+  let thirdLine = `  ${ingredient.quantity *= servings} ${ingredient.measurement} in total`
 
   console.log(firstLine)
   console.log(secondLine)
+  console.log(thirdLine)
 
   return `Servings: ${servings}`
 }
 
 // Uncomment the code below for Exercise 3
-// print({ name: 'chilli', quantity: 1, measurement: 'whole', preparation: 'chopped and deseeded', colour: 'red' }, 1)
+// print({ name: 'chilli', quantity: 1, measurement: 'whole', preparation: 'chopped and deseeded', colour: 'red' }, 2)
